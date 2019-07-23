8cea307ee4c9a5e357969dd34ba82c599eb62bb5
Mohamed Keita <mohkeita13@gmail.com>
2019-07-23 13:11:45 +0200

    data

--- backend/libs/data_base.php
+++ backend/libs/data_base.php
@@ -5,19 +5,3 @@
         error_reporting(0);
         // echo "connecté à la bd Avec DEBUG";
     }
-
-    $dbname   = "fruits_stock";
-    $host     = "localhost";
-    $charset  = "utf8";
-    $dbuser   = "root";
-    $dbpwd    = "";
-
-    try {
-        $bdd = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset", $dbuser, $dbpwd);
-    } catch (Exception $erreur) {
-        if (DEBUG) {
-            die("Erreur de connexion à la base de données" . $erreur->getMessage());
-        } else {
-            die("Erreur de connexion à la base de données, merci de contacter XXX");
-        }
-    }
