bd88fdfeee1384d47b7e0b2beda3be03f8ba31fb
Mohamed Keita <mohkeita13@gmail.com>
2019-07-24 09:19:34 +0200

    details

---
+++ backend/delete.php
@@ -0,0 +1,42 @@
+<?php
+// require headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// inclure la base de données et le fichier objet
+require_once './libs/data_base.php';
+require_once './objets/produits.php';
+
+
+
+// prépare produit
+$product = new Produits($bdd);
+
+// obtenir l'identifiant du produit
+$data = json_decode(file_get_contents("php://input"));
+
+// définition de l'identifiant du produit à supprimer
+$product->id = $data->id;
+
+// supprime le produit
+if ($product->delete()) {
+
+    // reponse code - 200 ok
+    http_response_code(200);
+
+    // Message en retour 
+    echo json_encode(array("message" => "Le produit a été supprimé."));
+}
+
+// si  c'est impossible de supprimer le produit
+else {
+
+    // reponse code - 503 Service indisponible
+    http_response_code(503);
+
+    // Message en retour au frontend
+    echo json_encode(array("message" => "Impossible de supprimer le produit."));
+}
--- backend/objets/produits.php
+++ backend/objets/produits.php
@@ -41,6 +41,15 @@ class Produits
 
         // query to read single record
         $query = "SELECT
+                 *
+             FROM
+                 " . $this->table_name . " 
+             WHERE
+                 id_produit = ?
+             LIMIT
+                 0,1";
+        /* requete a changer
+        $query = "SELECT
                  c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
              FROM
                  " . $this->table_name . " p
@@ -52,24 +61,50 @@ class Produits
              LIMIT
                  0,1";
 
+                 */
+
         // prepare query statement
         $stmt = $this->conn->prepare($query);
 
-        // bind id of product to be updated
-        $stmt->bindParam(1, $this->id);
+        // lier l'id du produit à mettre à jour
+        $stmt->bindParam(1, $this->id_produit);
 
         // execute query
         $stmt->execute();
 
-        // get retrieved row
+        // Recuprer les ligne 
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        // set values to object properties
+        // définir des valeurs pour les propriétés d'objet
         $this->name = $row['name'];
         $this->ref = $row['ref'];
         $this->qty = $row['qty'];
         $this->price = $row['price'];
-        $this->id_four = $row['id_fournisseur'];
-        $this->name_four = $row['name_fournisseur'];
+        // $this->id_four = $row['id_fournisseur'];
+        // $this->name_four = $row['name_fournisseur'];
+    }
+
+    // Suprimer le produit
+    function delete()
+    {
+
+        // sql query pour suprimer
+        $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+
+        // Proteger l'id et recuperer
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        // lier l'id de l'enregistrement à supprimer
+        $stmt->bindParam(1, $this->id);
+
+        // execute query
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
     }
 }
